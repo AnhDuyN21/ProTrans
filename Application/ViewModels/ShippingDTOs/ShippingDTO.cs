61f4e2676644894a1bad49c108a00bec52540c61
haocetu <nhathao230502@gmail.com>
2024-10-16 01:56:11 +0700

    Edit DTO

--- Application/ViewModels/ShippingDTOs/ShippingDTO.cs
+++ Application/ViewModels/ShippingDTOs/ShippingDTO.cs
@@ -6,6 +6,6 @@
         public Guid ShipperId { get; set; }
         public Guid OrderId { get; set; }
         public string ImageUrl { get; set; }
-        public bool IsShipped { get; set; }
+        public string Status { get; set; }
     }
 }
