456f5e17125dbd9f9d4626b27e99bc74979c6c04
nhatle1909 <nle75234@gmail.com>
2024-10-30 21:33:06 +0700

    FEAT : Role + FIX : AssignmentNotarization

--- Application/Interfaces/InterfaceRepositories/Role/IRoleRepository.cs
+++ Application/Interfaces/InterfaceRepositories/Role/IRoleRepository.cs
@@ -1,8 +1,11 @@
-﻿namespace Application.Interfaces.InterfaceRepositories.Role
+﻿using System.Linq.Expressions;
+
+namespace Application.Interfaces.InterfaceRepositories.Role
 {
     public interface IRoleRepository
     {
         string GetRoleName(Guid roleId);
         public Guid GetRoleIdByName(string roleName);
+        public Task<List<Domain.Entities.Role>> GetRoles(Expression<Func<Domain.Entities.Role, bool>>? filter = null, string? includeProperties = null);
     }
 }
---
+++ Application/Services/Role/NotificationService.cs
@@ -0,0 +1,53 @@
+﻿using Application.Commons;
+using Application.Interfaces;
+using Application.Interfaces.InterfaceServices.Account;
+using Application.ViewModels.RoleDTOs;
+using AutoMapper;
+using System.Data.Common;
+
+namespace Application.Services.role
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public RoleService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ServiceResponse<IEnumerable<RoleDTO>>> GetRolesAsync()
+        {
+            var response = new ServiceResponse<IEnumerable<RoleDTO>>();
+
+            try
+            {
+                var roleList = await _unitOfWork.RoleRepository.GetRoles(a => a.Name.Equals("Shipper") || a.Name.Equals("Staff"));
+                var roleDTOs = _mapper.Map<List<RoleDTO>>(roleList);
+
+                if (roleDTOs.Count != 0)
+                {
+                    response.Success = true;
+                    response.Message = "Role list retrieved successfully";
+                    response.Data = roleDTOs;
+                }
+                else
+                {
+                    response.Success = true;
+                    response.Message = "Not have role in list";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = "Error";
+                response.ErrorMessages = new List<string> { Convert.ToString(ex.Message) };
+            }
+
+            return response;
+        }
+    
+    }
+}
