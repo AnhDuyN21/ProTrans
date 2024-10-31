6650c1bc49791356986f21df7f1f95f5b10f74c4
haocetu <nhathao230502@gmail.com>
2024-10-31 23:47:07 +0700

    Document, Order Controller

--- WebAPI/Controllers/Documents/DocumentController.cs
+++ WebAPI/Controllers/Documents/DocumentController.cs
@@ -4,20 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.Documents
 {
-    public class DocumentController : BaseController
-    {
-        private readonly IDocumentService documentService;
-        public DocumentController(IDocumentService documentService)
-        {
-            this.documentService = documentService;
-        }
+	public class DocumentController : BaseController
+	{
+		private readonly IDocumentService documentService;
+		public DocumentController(IDocumentService documentService)
+		{
+			this.documentService = documentService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllDocuments()
-        {
-            var result = await documentService.GetAllDocumentsAsync();
-            return Ok(result);
-        }
+		[HttpGet]
+		public async Task<IActionResult> GetAllDocuments()
+		{
+			var result = await documentService.GetAllDocumentsAsync();
+			return Ok(result);
+		}
 
 		[HttpGet("GetDocumentsToBeNotarizated")]
 		public async Task<IActionResult> GetDocumentsToBeNotarizated()
@@ -26,62 +26,69 @@ namespace WebAPI.Controllers.Documents
 			return Ok(result);
 		}
 
+		[HttpGet("GetDocumentsToBeNotarizedByOrderId")]
+		public async Task<IActionResult> GetDocumentsToBeNotarizedByOrderId(Guid id)
+		{
+			var result = await documentService.GetDocumentsToBeNotarizedByOrderIdAsync(id);
+			return Ok(result);
+		}
+
 		[HttpGet("{id}")]
-        public async Task<IActionResult> GetDocumentById(Guid id)
-        {
-            var result = await documentService.GetDocumentByIdAsync(id);
-            if (!result.Success)
-            {
-                return NotFound(result);
-            }
-            return Ok(result);
-        }
+		public async Task<IActionResult> GetDocumentById(Guid id)
+		{
+			var result = await documentService.GetDocumentByIdAsync(id);
+			if (!result.Success)
+			{
+				return NotFound(result);
+			}
+			return Ok(result);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> CreateDocument([FromBody] CreateDocumentDTO Document)
-        {
-            var result = await documentService.CreateDocumentAsync(Document);
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return BadRequest(result);
-            }
-        }
+		[HttpPost]
+		public async Task<IActionResult> CreateDocument([FromBody] CreateDocumentDTO Document)
+		{
+			var result = await documentService.CreateDocumentAsync(Document);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			else
+			{
+				return BadRequest(result);
+			}
+		}
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateDocument(Guid id, [FromBody] UpdateDocumentDTO CUdocumentDTO)
-        {
-            var result = await documentService.UpdateDocumentAsync(id, CUdocumentDTO);
-            if (!result.Success)
-            {
-                return NotFound(result);
-            }
-            return Ok(result);
-        }
+		[HttpPut("{id}")]
+		public async Task<IActionResult> UpdateDocument(Guid id, [FromBody] UpdateDocumentDTO CUdocumentDTO)
+		{
+			var result = await documentService.UpdateDocumentAsync(id, CUdocumentDTO);
+			if (!result.Success)
+			{
+				return NotFound(result);
+			}
+			return Ok(result);
+		}
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteDocument(Guid id)
-        {
-            var result = await documentService.DeleteDocumentAsync(id);
-            if (!result.Success)
-            {
-                return NotFound(result);
-            }
-            return Ok(result);
-        }
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteDocument(Guid id)
+		{
+			var result = await documentService.DeleteDocumentAsync(id);
+			if (!result.Success)
+			{
+				return NotFound(result);
+			}
+			return Ok(result);
+		}
 
-        [HttpGet("GetByOrderId")]
-        public async Task<IActionResult> GetDocumentsByOrderId(Guid id)
-        {
-            var result = await documentService.GetDocumentsByOrderIdAsync(id);
-            if (!result.Success)
-            {
-                return NotFound(result);
-            }
-            return Ok(result);
-        }
-    }
+		[HttpGet("GetByOrderId")]
+		public async Task<IActionResult> GetDocumentsByOrderId(Guid id)
+		{
+			var result = await documentService.GetDocumentsByOrderIdAsync(id);
+			if (!result.Success)
+			{
+				return NotFound(result);
+			}
+			return Ok(result);
+		}
+	}
 }
