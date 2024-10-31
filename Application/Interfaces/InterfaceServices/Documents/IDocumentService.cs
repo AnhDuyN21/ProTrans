71514b50f565b71a5582c556796dfc188afdd55b
haocetu <nhathao230502@gmail.com>
2024-10-31 23:28:07 +0700

    GetDocumentsToBeNotarizedByOrderId

--- Application/Interfaces/InterfaceServices/Documents/IDocumentService.cs
+++ Application/Interfaces/InterfaceServices/Documents/IDocumentService.cs
@@ -3,14 +3,15 @@ using Application.ViewModels.DocumentDTOs;
 
 namespace Application.Interfaces.InterfaceServices.Documents
 {
-    public interface IDocumentService
-    {
-        public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetAllDocumentsAsync();
-        public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsToBeNotarizatedAsync();
-        public Task<ServiceResponse<DocumentDTO>> GetDocumentByIdAsync(Guid id);
-        public Task<ServiceResponse<DocumentDTO>> UpdateDocumentAsync(Guid id, UpdateDocumentDTO document);
-        public Task<ServiceResponse<DocumentDTO>> CreateDocumentAsync(CreateDocumentDTO document);
-        public Task<ServiceResponse<bool>> DeleteDocumentAsync(Guid id);
-        public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsByOrderIdAsync(Guid id);
-    }
+	public interface IDocumentService
+	{
+		public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetAllDocumentsAsync();
+		public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsToBeNotarizatedAsync();
+		public Task<ServiceResponse<DocumentDTO>> GetDocumentByIdAsync(Guid id);
+		public Task<ServiceResponse<DocumentDTO>> UpdateDocumentAsync(Guid id, UpdateDocumentDTO document);
+		public Task<ServiceResponse<DocumentDTO>> CreateDocumentAsync(CreateDocumentDTO document);
+		public Task<ServiceResponse<bool>> DeleteDocumentAsync(Guid id);
+		public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsByOrderIdAsync(Guid id);
+		public Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsToBeNotarizedByOrderIdAsync(Guid id);
+	}
 }
--- Application/Services/Documents/DocumentService.cs
+++ Application/Services/Documents/DocumentService.cs
@@ -255,5 +255,36 @@ namespace Application.Services.Documents
 			}
 			return response;
 		}
+
+		public async Task<ServiceResponse<IEnumerable<DocumentDTO>>> GetDocumentsToBeNotarizedByOrderIdAsync(Guid id)
+		{
+			var response = new ServiceResponse<IEnumerable<DocumentDTO>>();
+
+			try
+			{
+				var documents = await _unitOfWork.DocumentRepository.GetByOrderIdAsync(id);
+				var targetDocuments = documents.Where(x => x.NotarizationRequest).ToList();
+				var documentDTOs = _mapper.Map<List<DocumentDTO>>(documents);
+
+				if (documentDTOs.Count != 0)
+				{
+					response.Success = true;
+					response.Message = "Get successfully.";
+					response.Data = documentDTOs;
+				}
+				else
+				{
+					response.Success = true;
+					response.Message = "No document exists.";
+				}
+			}
+			catch (Exception ex)
+			{
+				response.Success = false;
+				response.Message = "Error.";
+				response.ErrorMessages = new List<string> { Convert.ToString(ex.Message) };
+			}
+			return response;
+		}
 	}
 }
