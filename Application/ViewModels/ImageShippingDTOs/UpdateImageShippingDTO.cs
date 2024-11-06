5141ec01295bc4c9dfbbff29d9cefd2de5fb2548
haocetu <nhathao230502@gmail.com>
2024-11-06 23:59:32 +0700

    ImageShipping DTO

---
+++ Application/ViewModels/ImageShippingDTOs/UpdateImageShippingDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.ImageShippingDTOs
+{
+	public class UpdateImageShippingDTO
+	{
+		public string? UrlPath { get; set; }
+	}
+}
