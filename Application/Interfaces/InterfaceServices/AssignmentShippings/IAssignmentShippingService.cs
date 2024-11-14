2d96773f4384bb9beb98f79ef8489b89fee04711
haocetu <nhathao230502@gmail.com>
2024-11-14 19:57:30 +0700

    Tách 2 API tạo AssignmentShipping

--- Application/Interfaces/InterfaceServices/AssignmentShippings/IAssignmentShippingService.cs
+++ Application/Interfaces/InterfaceServices/AssignmentShippings/IAssignmentShippingService.cs
@@ -8,7 +8,9 @@ namespace Application.Interfaces.InterfaceServices.AssignmentShippings
 		public Task<ServiceResponse<IEnumerable<AssignmentShippingDTO>>> GetAllAssignmentShippingsAsync();
 		public Task<ServiceResponse<AssignmentShippingDTO>> GetAssignmentShippingByIdAsync(Guid id);
 		public Task<ServiceResponse<AssignmentShippingDTO>> UpdateAssignmentShippingAsync(Guid id, UpdateAssignmentShippingDTO assignmentShipping);
-		public Task<ServiceResponse<AssignmentShippingDTO>> CreateAssignmentShippingAsync(CreateAssignmentShippingDTO assignmentShipping);
+		//public Task<ServiceResponse<AssignmentShippingDTO>> CreateAssignmentShippingAsync(CreateAssignmentShippingDTO assignmentShipping);
+		public Task<ServiceResponse<AssignmentShippingDTO>> CreateAssignmentShippingToShipAsync(CreateAssignmentShippingDTO assignmentShipping);
+		public Task<ServiceResponse<AssignmentShippingDTO>> CreateAssignmentShippingToPickUpAsync(CreateAssignmentShippingDTO assignmentShipping);
 		public Task<ServiceResponse<bool>> DeleteAssignmentShippingAsync(Guid id);
 		public Task<ServiceResponse<IEnumerable<AssignmentShippingDTO>>> GetAssignmentShippingsByShipperIdAsync(Guid id);
 		public Task<ServiceResponse<IEnumerable<AssignmentShippingDTO>>> GetShipAssignmentShippingsByShipperIdAsync(Guid id);
