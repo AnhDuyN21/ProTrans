e6497362a47db605c0005b48fad53ef53870d267
haocetu <nhathao230502@gmail.com>
2024-11-14 19:10:24 +0700

    GetCompletedOrders

--- Application/Services/Orders/OrderService.cs
+++ Application/Services/Orders/OrderService.cs
@@ -116,7 +116,7 @@ namespace Application.Services.Orders
 			try
 			{
 				var orders = await _unitOfWork.OrderRepository.GetAllAsync();
-				var completedOrders = orders.Where(order => order.Status == "Completed").ToList();
+				var completedOrders = orders.Where(order => order.ShipRequest && order.Status == "Completed").ToList();
 				var orderDTOs = _mapper.Map<List<OrderDTO>>(completedOrders);
 
 				if (orderDTOs.Count != 0)
