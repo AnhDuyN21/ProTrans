b19eeff86af0ac1a28f2a37cc211779246e4c17c
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-11-27 01:38:45 +0700

    fix: UpdateRequestAsync

--- Application/ViewModels/RequestDTOs/UpdateRequestDTO.cs
+++ Application/ViewModels/RequestDTOs/UpdateRequestDTO.cs
@@ -8,7 +8,6 @@ namespace Application.ViewModels.RequestDTOs
     public class UpdateRequestDTO
     {
         public DateTime? Deadline { get; set; }
-        [JsonIgnore]
         public decimal? EstimatedPrice { get; set; }
         [EnumDataType(typeof(RequestStatus),ErrorMessage = "Trạng thái cập nhật của request phải là Quoted hoặc Cancel")]
         public string? Status { get; set; }
@@ -16,6 +15,5 @@ namespace Application.ViewModels.RequestDTOs
         public bool? PickUpRequest { get; set; }
         [JsonIgnore]
         public bool ShipRequest { get; set; }
-        public List<UpdateDocumentFromRequestDTO>? Documents { get; set; }
     }
 }
