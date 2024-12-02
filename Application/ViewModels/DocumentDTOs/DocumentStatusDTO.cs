77e8a9c4a5c8b56f11ee567e5ed38cbee0a9af93
haocetu <nhathao230502@gmail.com>
2024-12-02 23:20:58 +0700

    Format AssignmentShippingService + DocumentStatusDTO

---
+++ Application/ViewModels/DocumentDTOs/DocumentStatusDTO.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.DocumentDTOs
+{
+	public class DocumentStatusDTO
+	{
+		public Guid Id { get; set; }
+		public string? Type { get; set; }
+		public string? Status { get; set; }
+		public DateTime? Time { get; set; }
+		public Guid? DocumentId { get; set; }
+		public virtual Document? Document { get; set; }
+	}
+}
