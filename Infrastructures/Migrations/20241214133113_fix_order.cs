632e55cd154383a7b99986984e0e4ac95d043279
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-12-14 20:32:59 +0700

    refactor: add migration
    add field pick up request to order

---
+++ Infrastructures/Migrations/20241214133113_fix_order.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_order : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PickUpRequest",
+                table: "Order",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PickUpRequest",
+                table: "Order");
+        }
+    }
+}
