8762abb8411fc39b789ef2abea0a3d313a75fbd3
haocetu <nhathao230502@gmail.com>
2024-12-14 21:17:24 +0700

    Update OrderService

--- Domain/Entities/Request.cs
+++ Domain/Entities/Request.cs
@@ -1,20 +1,19 @@
 ﻿namespace Domain.Entities
 {
-    public class Request : BaseEntity
-    {
-        //Field
-        public DateTime? Deadline { get; set; }
-        public decimal? EstimatedPrice { get; set; }
-        public string? Status { get; set; }
-        public bool? IsConfirmed { get; set; } 
-        public bool? PickUpRequest { get; set; } 
-        public bool ShipRequest { get; set; } 
-        //Foreignkey
-        public Guid? CustomerId { get; set; }
-        //Relationship
-        public virtual Account? CustomerAccount { get; set; }
-        public virtual ICollection<Document>? Documents { get; set; }
-        public virtual Order? Order { get; set; }
-
-    }
+	public class Request : BaseEntity
+	{
+		//Field
+		public DateTime? Deadline { get; set; }
+		public decimal? EstimatedPrice { get; set; }
+		public string? Status { get; set; }
+		public bool? IsConfirmed { get; set; }
+		public bool? PickUpRequest { get; set; }
+		public bool ShipRequest { get; set; }
+		//Foreignkey
+		public Guid? CustomerId { get; set; }
+		//Relationship
+		public virtual Account? CustomerAccount { get; set; }
+		public virtual ICollection<Document>? Documents { get; set; }
+		public virtual Order? Order { get; set; }
+	}
 }
