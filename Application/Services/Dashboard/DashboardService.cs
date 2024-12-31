59c9e314fe2896950e56787a3f677b2dfecb509b
haocetu <nhathao230502@gmail.com>
2024-12-31 15:17:31 +0700

    DashboardService

---
+++ Application/Services/Dashboard/DashboardService.cs
@@ -0,0 +1,44 @@
+﻿using Application.Commons;
+using Application.Interfaces;
+using Application.Interfaces.InterfaceServices.Dashboard;
+using Application.ViewModels.Dashboard;
+using Application.ViewModels.DistanceDTOs;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Dashboard
+{
+	public class DashboardService : IDashboardService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+		public DashboardService(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+		}
+
+		public async Task<ServiceResponse<DashboardDTO>> Get()
+		{
+			var response = new ServiceResponse<DashboardDTO>();
+
+			if (getById == null)
+			{
+				response.Success = false;
+				response.Message = "Id is not existed";
+			}
+			else
+			{
+				response.Success = true;
+				response.Message = "Agency found";
+				response.Data = _mapper.Map<DistanceDTO>(getById);
+			}
+
+			return response;
+		}
+	}
+}
---
+++ Application/ViewModels/Dashboard/DashboardDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.Dashboard
+{
+	public class DashboardDTO
+	{
+		public int? NumberOfAccounts { get; set; }
+		public int? NumberOfOrders { get; set; }
+		public int? DailyRevenue { get; set; }
+		public int? WeeklyRevenue { get; set; }
+		public int? MonthlyRevenue { get; set; }
+		public int? AnnualRevenue { get; set; }
+	}
+}
