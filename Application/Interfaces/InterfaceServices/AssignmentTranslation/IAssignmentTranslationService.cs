3bdc9ff0510227252c024a9c83dc11e3732475d5
haocetu <nhathao230502@gmail.com>
2024-12-31 14:50:47 +0700

    AssignmentTranslation

--- Application/Interfaces/InterfaceServices/AssignmentTranslation/IAssignmentTranslationService.cs
+++ Application/Interfaces/InterfaceServices/AssignmentTranslation/IAssignmentTranslationService.cs
@@ -4,15 +4,14 @@ using Domain.Enums;
 
 namespace Application.Interfaces.InterfaceServices.AssignmentTranslation
 {
-    public interface IAssignmentTranslationService
-    {
-        public Task<ServiceResponse<IEnumerable<AssignmentTranslationDTO>>> GetAllAssignmentTranslationsAsync();
-        public Task<ServiceResponse<IEnumerable<AssignmentTranslationByTranslatorIdDTO>>> GetAllAssignmentTranslationByTranslatorIdAsync(Guid Id);
-        public Task<ServiceResponse<IEnumerable<AssignmentTranslationByTranslatorIdDTO>>> GetAssignmentTranslationByTranslatorIdAndStatusAsync(Guid Id,string status);
-        public Task<ServiceResponse<AssignmentTranslationDTO>> UpdateAssignmentTranslationAsync(Guid id, CUAssignmentTranslationDTO cudAssignmentTranslationDTO);
-        public Task<ServiceResponse<AssignmentTranslationDTO>> UpdateStatusAssignmentTranslationAsync(Guid id, string? urlPath);
-       
-        public Task<ServiceResponse<AssignmentTranslationDTO>> CreateAssignmentTranslationAsync(CUAssignmentTranslationDTO AssignmentTranslationDTO);
-        public Task<ServiceResponse<bool>> DeleteAssignmentTranslationAsync(Guid id);
-    }
+	public interface IAssignmentTranslationService
+	{
+		public Task<ServiceResponse<IEnumerable<AssignmentTranslationDTO>>> GetAllAssignmentTranslationsAsync();
+		public Task<ServiceResponse<IEnumerable<AssignmentTranslationByTranslatorIdDTO>>> GetAllAssignmentTranslationByTranslatorIdAsync(Guid Id);
+		public Task<ServiceResponse<IEnumerable<AssignmentTranslationByTranslatorIdDTO>>> GetAssignmentTranslationByTranslatorIdAndStatusAsync(Guid Id, string status);
+		public Task<ServiceResponse<AssignmentTranslationDTO>> UpdateAssignmentTranslationAsync(Guid id, CUAssignmentTranslationDTO cudAssignmentTranslationDTO);
+		public Task<ServiceResponse<AssignmentTranslationDTO>> UpdateStatusAssignmentTranslationAsync(Guid id, string? urlPath);
+		public Task<ServiceResponse<AssignmentTranslationDTO>> CreateAssignmentTranslationAsync(CUAssignmentTranslationDTO AssignmentTranslationDTO);
+		public Task<ServiceResponse<bool>> DeleteAssignmentTranslationAsync(Guid id);
+	}
 }
